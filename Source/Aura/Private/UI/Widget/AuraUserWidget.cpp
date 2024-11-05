c2ac141924e23e5fc70a4d24edbdbd9d1c9de6e0
CieranNicholas <47529890+CieranNicholas@users.noreply.github.com>
2024-11-05 01:29:29 +0000

    Base UserWidget & Widget controller classes added.

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Cieran Nicholas
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright Cieran Nicholas
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
