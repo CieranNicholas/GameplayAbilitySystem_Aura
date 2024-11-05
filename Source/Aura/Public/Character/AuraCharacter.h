1b6e06f455177774fba69a29d946d6c7419b68ef
CieranNicholas <47529890+CieranNicholas@users.noreply.github.com>
2024-11-05 00:16:35 +0000

    Calling InitAbilityActorInfo() on the relevant characters.

--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -14,5 +14,13 @@ class AURA_API AAuraCharacter : public AAuraCharacterBase
 {
 	GENERATED_BODY()
 
+
+public:
 	AAuraCharacter();
+	virtual void PossessedBy(AController* NewController) override;
+	virtual void OnRep_PlayerState() override;
+
+protected:
+private:
+	void InitAbilityActorInfo();
 };
--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -15,9 +15,14 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
 
-	AAuraEnemy();
 	
 public:
+	AAuraEnemy();
+
+	/** Enemy Interface */
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
+	/** End Enemy Interface */
+protected:
+	virtual void BeginPlay() override; 
 };
